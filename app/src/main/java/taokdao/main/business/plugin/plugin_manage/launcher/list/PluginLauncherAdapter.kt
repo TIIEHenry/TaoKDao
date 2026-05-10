32e7fbdeedd3555d1849ebe6382c4479b5927aa5
clarence <tiiehenry@qq.com>
2026-05-10 09:55:37 +0800

    fix(plugin): 修正插件启动器列表布局错用问题
    - 将布局文件插件启动器列表项从列表布局修改为网格布局
    - 更新绑定类从 PluginManageLauncherListItemListBinding 切换到 PluginManageLauncherListItemGirdBinding
    - 修正 getItemLayoutId 返回的布局资源为网格布局资源
    - 适配数据绑定逻辑以匹配新的网格布局绑定类

--- app/src/main/java/taokdao/main/business/plugin/plugin_manage/launcher/list/PluginLauncherAdapter.kt
+++ app/src/main/java/taokdao/main/business/plugin/plugin_manage/launcher/list/PluginLauncherAdapter.kt
@@ -5,13 +5,13 @@ import taokdao.api.plugin.load.IPluginLoader
 import tiiehenry.android.view.recyclerview.adapter.BaseIdRecyclerAdapter
 import tiiehenry.android.view.recyclerview.holder.RecyclerViewHolder
 import tiiehenry.ideditor.R
-import tiiehenry.ideditor.databinding.PluginManageLauncherListItemListBinding
+import tiiehenry.ideditor.databinding.PluginManageLauncherListItemGirdBinding
 
 class PluginLauncherAdapter(private val pluginLoader: IPluginLoader) :
     BaseIdRecyclerAdapter<PluginLauncherItem>() {
     override fun getItemLayoutId(viewType: Int) = R.layout.plugin_manage_launcher_list_item_gird
     override fun bindData(holder: RecyclerViewHolder, position: Int, item: PluginLauncherItem) {
-        val binding = holder.bindingOf<PluginManageLauncherListItemListBinding>()
+        val binding = holder.bindingOf<PluginManageLauncherListItemGirdBinding>()
         val icon = item.information.icon ?: item.plugin?.let { pluginLoader.loadPluginIcon(it) }
         binding.ivIcon.setImageDrawable(
             icon
